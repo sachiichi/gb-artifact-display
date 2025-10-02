f74962bef141dc43bca542a10ba432497caab0aa
Sachiichi <sachiichi@schch.net>
2025-10-03 02:31:28 +0900

    HARファイル読み込みに変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,15 @@
 import ArtifactInputDialog from "@/components/ArtifactInputDialog";
 import ArtifactTable from "@/components/ArtifactTable";
 import GBFArtifactApiResponse, { GBFArtifact } from "@/types/artifact";
-import { Button, Stack, TextField, Typography } from "@mui/material";
+import { AppBar, Box, Button, IconButton, Stack, TextField, Toolbar, Typography } from "@mui/material";
 import React from "react";
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 
 
 export default function Home() {
   const [artifactJson, setArtifactJson] = React.useState<GBFArtifact[]>([]);
   const [open, setOpen] = React.useState(false);
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+
 
 
   const handleClose = (value?: string) => {
@@ -26,15 +25,37 @@ export default function Home() {
   };
 
   return (
-    <Stack>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        アーティファクトの入力画面を開く
-      </Button>
-      <ArtifactInputDialog
-        open={open}
-        onClose={handleClose}
-      />
-      {artifactJson.length>0 && <ArtifactTable artifactData={artifactJson}/>}
-    </Stack>
+    <Box>
+      <AppBar position="static">
+        <Toolbar>
+          {/* <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton> */}
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            アーティファクト眺めるやつ
+          </Typography>
+          <Button color="inherit"></Button>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <HelpOutlineIcon/>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Stack>
+        {artifactJson.length === 0 && <ArtifactInputDialog setArtifactJson={setArtifactJson} />}
+        {artifactJson.length > 0 && <ArtifactTable artifactData={artifactJson} />}
+      </Stack>
+    </Box>
   );
 }
